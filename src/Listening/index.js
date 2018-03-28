4abeff2ec6a2cd8f2ae464f9ce958bc1f162a850
Tim Kholod <artijoh@gmail.com>
2018-03-28 14:24:34 +0800

    Change listening section icon

---
+++ src/Listening/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+class Listening extends Component {
+  state = {};
+
+  render() {
+    return <div>Listening</div>;
+  }
+}
+
+export default Listening;
