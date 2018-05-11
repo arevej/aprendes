a37bb4fadedc39e7295a420a88b5c45d2a14e5af
Tim Kholod <artijoh@gmail.com>
2018-05-11 09:52:14 +0200

    Add react router transition

--- src/Course/index.js
+++ src/Course/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 
 import Header from '../Header';
 import Container from '../Container';
